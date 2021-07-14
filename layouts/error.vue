1249157573a19c4bf9cac0e09766b227851295e2
fearhunt <firhanmahdi@gmail.com>
2021-07-15 00:41:23 +0700

    init commti

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="main-content">
+    <!-- <RedirectBack :message="message" /> -->
+  </main>
+</template>
+
+<script>
+  export default {
+    props: [ 
+      "error" 
+    ],
+
+    data() {
+      return {
+        message: (this.error.statusCode === 404) ? "Laman yang Anda akses tidak dapat ditemukan." : "Terdapat error."
+      }
+    },
+
+    layout: "error"
+  }
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [
+
+  ],
+
+  darkMode: false, // or 'media' or 'class'
+
+  important: false,
+
+  theme: {
+    extend: {
+
+    },
+  },
+
+  variants: {
+    extend: {
+
+    },
+  },
+
+  plugins: [
+
+  ],
+}
