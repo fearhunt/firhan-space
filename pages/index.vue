05684e837b2a64659ae811780a42bec3dd3e0ec8
fearhunt <firhanmahdi@gmail.com>
2021-12-15 22:41:20 +0700

    Coming soon update

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-
+  <div class="flex items-center h-screen w-screen">
+    <div class="text-center min-w-full">
+      <h1 class="text-6xl">
+        🥳
+      </h1>
+      <br>
+      <h2 class="text-3xl font-semibold">
+        Coming Soon // Segera Tayang!
+      </h2>
+      <hr class="my-4 w-48 border-2 border-gray-700 mx-auto">
+      <img :src="require('~/assets/img/fmr.png')" class="block logo-fmr w-16 mx-auto" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +21,8 @@
 </script>
 
 <style lang="scss">
-  
+  .mx-auto {
+    margin-left: auto;
+    margin-right: auto;
+  }
 </style>
